ddcec4aac738090e5da00778a7f5add02891d842
Tyler McDaniel <bmcdan16@utk.edu>
2021-01-20 11:14:51 -0500

    Revert "Add macro to slate tests"
    This reverts commit 21793fedd653e69434766520b94162f3b99513f7.

--- src/fast_math_tests.cpp
+++ src/fast_math_tests.cpp
@@ -795,7 +795,6 @@ TEMPLATE_TEST_CASE("LU Routines", "[fast_math]", float, double)
     rmse_comparison(x, X1_gold, tol_factor);
   }
 
-#ifdef ASGARD_USE_SLATE
   SECTION("slate_gesv and slate_getrs")
   {
     fk::matrix<TestType> const A_copy = A_gold;
@@ -813,5 +812,4 @@ TEMPLATE_TEST_CASE("LU Routines", "[fast_math]", float, double)
     fm::getrs(A_copy, x, ipiv, solve_opts::slate);
     rmse_comparison(x, X1_gold, tol_factor);
   }
-#endif
 }
